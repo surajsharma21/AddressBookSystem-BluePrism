55a2f70def1c63fa448eb048cc8519917a12e953
Suraj <surajshrm000@gmail.com>
2020-11-16 22:19:23 +0530

    [Suraj] Add: Query to create a table in database

---
+++ CreateTableQuery.sql
@@ -0,0 +1,11 @@
+create table AddressBook
+(
+    first_name varchar(30),
+    last_name varchar(30),
+    address varchar(50),
+    city varchar(20),
+    state varchar(20),
+    zip int,
+    phone_number varchar(13),
+    email varchar(20)
+);
